f835b73636dd88009157f73b61e942a7fd829ef7
weverthondev <weverthondev@gmail.com>
2026-03-12 01:19:33 -0300

    fix: corrige ordem de inicialização do asgi

--- taxifila/asgi.py
+++ taxifila/asgi.py
@@ -1,16 +1,17 @@
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taxifila.settings')
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import core.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taxifila.settings')
+from core.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(
-        URLRouter(
-            core.routing.websocket_urlpatterns
-        )
+        URLRouter(websocket_urlpatterns)
     ),
 })
